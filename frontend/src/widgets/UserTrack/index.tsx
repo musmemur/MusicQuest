55725614dfa513e15015ef80195b5d99285adcea
musmemur <timurkrivoscheev2005@gmail.com>
2025-06-16 00:21:12 +0300

    .

--- frontend/src/widgets/UserTrack/index.tsx
+++ frontend/src/widgets/UserTrack/index.tsx
@@ -2,8 +2,10 @@ import './index.css';
 import './adaptive.css';
 import type { Track } from "../../entities/Track.ts";
 import { PausedTrack } from "../../shared/assets/svg/PausedTrack.tsx";
+import {useState} from "react";
 
 export const UserTrack = ({ track }: { track: Track }) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
 
     const openInDeezer = () => {
         window.open(`https://www.deezer.com/track/${track.deezerTrackId}`, '_blank');
@@ -11,19 +13,28 @@ export const UserTrack = ({ track }: { track: Track }) => {
 
     return (
         <div className="user-track-container">
-            <div style={{ position: 'relative' }}>
-                <img src={track.coverUrl} alt={`Cover: ${track.title}`} />
-                <button
-                    className="play-button"
-                    onClick={openInDeezer}
-                    disabled={!track.previewUrl}
-                >
-                    <PausedTrack />
-                </button>
+            <div className="user-track-photo-container">
+                <img
+                    src={track.coverUrl}
+                    alt={`Cover: ${track.title}`}
+                    onLoad={() => setImageLoaded(true)}
+                    style={{ display: imageLoaded ? 'block' : 'none' }}
+                />
+                {imageLoaded && (
+                    <button
+                        className="play-button"
+                        onClick={openInDeezer}
+                        disabled={!track.previewUrl}
+                    >
+                        <PausedTrack />
+                    </button>
+                )}
             </div>
-            <div className="user-track-info">
-                <span>{track.artist} — {track.title}</span>
-            </div>
+            {imageLoaded && (
+                <div className="user-track-info">
+                    <span>{track.artist} — {track.title}</span>
+                </div>
+            )}
         </div>
     );
 };
